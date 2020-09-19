a2365b3052d1c60a3edc1977c51f7b6fe0f7cf33
Jon <stricoff92@gmail.com>
2020-09-19 14:41:26 -0400

    wip

--- metalacc/api/forms/company.py
+++ metalacc/api/forms/company.py
@@ -1,7 +1,10 @@
 
 from django import forms
+from django.core.signing import BadSignature
+import jwt
 
 from api.models import Company
+from api.lib import company_export
 
 
 class CompanyForm(forms.ModelForm):
@@ -13,3 +16,16 @@ class CompanyForm(forms.ModelForm):
 class CompanySelectionForm(forms.Form):
     company = forms.ModelChoiceField(
         queryset=Company.objects.all(), required=True, to_field_name="slug")
+
+
+class ImportCompanyForm(forms.Form):
+    data = forms.CharField(required=True)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        try:
+            cleaned_data['decoded_data'] = company_export.decode_signed_jwt(cleaned_data['data'])
+        except (BadSignature, jwt.InvalidSignatureError):
+            raise forms.ValidationError("invalid data")
+        
+        return cleaned_data
---
+++ metalacc/api/lib/company_export.py
@@ -0,0 +1,133 @@
+
+from api.models import Company, Account, Period, JournalEntry, JournalEntryLine
+
+import jwt
+from django.conf import settings
+from django.core.signing import Signer
+from django.db.models.functions import Cast
+from django.db.models import CharField
+from django.db import transaction
+
+from api.utils import generate_slug, generate_slugs_batch
+
+
+signer = Signer(key=settings.OBJECT_SIGNING_KEY)
+
+# EXPORT FUNCTIONS # # # # # # # # # #
+
+def export_company_to_jwt(company):
+    data = {
+        'version':settings.OBJECT_SERIALIZATION_VERSION,
+    }
+
+    # Serialize company.
+    data['company'] = {"name":company.name}
+
+    # Serialize periods
+    data['periods'] = list(company.period_set
+        .values("id")
+        .annotate(
+            start_str=Cast('start', CharField()),
+            end_str=Cast('end', CharField())
+        ))
+
+    # Serialize accounts
+    data['accounts'] = list(company.account_set.values(
+        "id", "name", "number", "type", "tag", "is_contra", "is_current", "is_operating"))
+    
+    # serialize journal entries
+    data['journal_entries'] = list(JournalEntry.objects
+        .filter(period__company=company).values(
+            "id", "period_id", "memo", "is_adjusting_entry", "is_closing_entry", "display_id")
+        .annotate(date_str=Cast("date", CharField())))
+    
+    # Serialize journal entry lines
+    data['journal_entry_lines'] = list(JournalEntryLine.objects
+        .filter(journal_entry__period__company=company)
+        .values(
+            "journal_entry_id", "account_id", "type", "amount"))
+
+    return jwt.encode(data, settings.OBJECT_SERIALIZATION_KEY, algorithm=settings.JWT_ALGORITHM).decode()
+
+
+def sign_comapny_export_jwt(jwt:str):
+    return signer.sign(jwt)
+
+
+
+# IMPORT FUNCTIONS # # # # # # #
+
+def decode_signed_jwt(signed_jwt:str):
+    encoded_jwt = signer.unsign(signed_jwt)
+    return jwt.decode(
+        encoded_jwt,
+        settings.OBJECT_SERIALIZATION_KEY,
+        algorithms=[settings.JWT_ALGORITHM])
+
+
+
+@transaction.atomic
+def import_company_data(data:dict, user) -> Company:
+    # TODO: use bulk_create
+
+    # Create a new company
+    company_name = data['company']['name']
+    if Company.objects.filter(user=user, name=company_name).exists():
+        company_name = company_name[:80] + generate_slug(None)
+
+    new_company = Company.objects.create(user=user, name=company_name)
+    data['company']['new_id'] = new_company.id
+
+    # create new accounts
+    account_map = {}
+    for ix, account_data in enumerate(data['accounts']):
+        new_account = Account.objects.create(
+            company=new_company,
+            user=user,
+            name=account_data['name'],
+            number=account_data['number'],
+            type=account_data['type'],
+            tag=account_data['tag'],
+            is_contra=account_data['is_contra'],
+            is_current=account_data['is_current'],
+            is_operating=account_data['is_operating'])
+
+        account_map[account_data['id']] = new_account
+
+
+    # Create new periods
+    period_map = {}
+    for ix, period_data in enumerate(data['periods']):
+        new_period = Period.objects.create(
+            start=period_data['start_str'], end=period_data['end_str'], company=new_company)
+        
+        period_map[period_data['id']] = new_period
+    
+
+    # Create new Journal Entries
+    journal_entry_map = {}
+    for ix, journal_entry_data in enumerate(data['journal_entries']):
+        new_journal_entry = JournalEntry.objects.create(
+            period=period_map[journal_entry_data['period_id']],
+            date=journal_entry_data['date_str'],
+            memo=journal_entry_data['memo'],
+            is_adjusting_entry=journal_entry_data['is_adjusting_entry'],
+            is_closing_entry=journal_entry_data['is_closing_entry'],
+        )
+        journal_entry_map[journal_entry_data['id']] = new_journal_entry
+
+    # Create journal entry lines
+    new_journal_entry_lines = []
+    jel_slugs = list(generate_slugs_batch(JournalEntryLine, len(data['journal_entry_lines'])))
+
+    for ix, jel_data in enumerate(data['journal_entry_lines']):
+        new_journal_entry_lines.append(JournalEntryLine(
+            slug=jel_slugs[ix],
+            journal_entry=journal_entry_map[jel_data['journal_entry_id']],
+            account=account_map[jel_data['account_id']],
+            type=jel_data['type'],
+            amount=jel_data['amount'],
+        ))
+    JournalEntryLine.objects.bulk_create(new_journal_entry_lines)
+
+    return new_company
