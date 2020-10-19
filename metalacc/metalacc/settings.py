c123f94a2a118d75f5ab792143cf02b2846f3142
Jon <stricoff92@gmail.com>
2020-10-18 20:11:15 -0400

    wip

--- metalacc/metalacc/settings.py
+++ metalacc/metalacc/settings.py
@@ -169,7 +169,14 @@ REST_FRAMEWORK = {
 # EMAIL SETTINGS
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+elif ENV == 'PROD':
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = applocals.EMAIL_HOST_USER
+    EMAIL_HOST_PASSWORD = applocals.EMAIL_HOST_PASSWORD
+    EMAIL_USE_TLS = True
+    DEFAULT_FROM_EMAIL = 'noreply@metalaccounting.com'
 
 
 
