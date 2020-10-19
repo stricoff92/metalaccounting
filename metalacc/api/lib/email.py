4b775ea50cc5e2655500b6ea7b110e8d957da367
Jon <stricoff92@gmail.com>
2020-10-19 18:47:49 -0400

    wip

--- metalacc/api/lib/email.py
+++ metalacc/api/lib/email.py
@@ -6,7 +6,7 @@ from api.utils import get_account_activation_url
 def send_account_activation_email(user, token:str):
     activation_url = get_account_activation_url(user.userprofile.slug, token)
 
-    from_email = None
+    from_email = 'no-reply@metalaccounting.com'
     to_emails = [user.email]
     email_message = f"Your MetalAccounting profile is ready. Click to activate your account: {activation_url}"
     send_mail(
